be5b99961ca3dc23c6ef5deef14f4495b305445e
Denis Dyakonov <holosty@yandex.ru>
2024-02-18 16:22:54 +0300

    Добавляет вывод в консоль результатов работы функций. Для проверки расскомментируйте строки.

--- js/functions.js
+++ js/functions.js
@@ -2,6 +2,8 @@ const checkStringLenght = (string, length) => string.length <= length;
 
 checkStringLenght('проверяемая строка', 20); // true
 checkStringLenght('проверяемая строка', 10); // false
+// console.log(checkStringLenght('проверяемая строка', 20));
+// console.log(checkStringLenght('проверяемая строка', 10));
 
 const isPalindrome = (string) => {
   const normalizedString = string.toLowerCase().replaceAll(' ', '');
@@ -15,6 +17,9 @@ const isPalindrome = (string) => {
 isPalindrome('ДовОд'); // true
 isPalindrome('Кекс'); // false
 isPalindrome('Лёша на полке клопа нашёл '); // true
+// console.log(isPalindrome('ДовОд'));
+// console.log(isPalindrome('Кекс'));
+// console.log(isPalindrome('Лёша на полке клопа нашёл '));
 
 const getNumber = (symbols) => {
   const symbolsString = symbols.toString();
@@ -35,3 +40,10 @@ getNumber('а я томат'); // NaN
 getNumber(2023); // 2023
 getNumber(-1); // 1
 getNumber(1.5); // 15
+// console.log(getNumber('2023 год'));
+// console.log(getNumber('ECMAScript 2022'));
+// console.log(getNumber('1 кефир, 0.5 батона'));
+// console.log(getNumber('а я томат'));
+// console.log(getNumber(2023));
+// console.log(getNumber(-1));
+// console.log(getNumber(1.5));
