6ad4c3d3052b3db86500e776b9bec55cdaa313e5
Denis Dyakonov <holosty@yandex.ru>
2024-02-27 23:48:32 +0300

    Создание функции счетчика

--- js/main.js
+++ js/main.js
@@ -36,6 +36,12 @@ const NAMES = [
 ];
 
 const NUMBER_OF_POSTS = 25;
+const MIN_AVATAR_NUMBER = 1;
+const MAX_AVATAR_NUMBER = 6;
+const MIN_QUANTITY_COMMENTS = 0;
+const MAX_QUANTITY_COMMENTS = 30;
+const MIN_QUANTITY_LIKES = 15;
+const MAX_QUANTITY_LIKES = 200;
 
 // Получение случайного целого числа в заданном диапазоне
 const getRandomInteger = (a, b) => {
@@ -48,27 +54,40 @@ const getRandomInteger = (a, b) => {
 // Получение случайного значения из не пустого массива
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
+// Создание ID
+const createIdGenerator = () => {
+  let counterId = 0;
+  return function () {
+    counterId++;
+    return counterId;
+  };
+};
+
+const createCommentId = createIdGenerator();
+const createPostId = createIdGenerator();
+const createUrlPhotoId = createIdGenerator();
+
 // Создание объекта комментария
-const createComment = (index) => ({
-  id: index,
-  avatar: `img/avatar-${ getRandomInteger(1, 6) }.svg`,
+const createComment = () => ({
+  id: createCommentId(),
+  avatar: `img/avatar-${ getRandomInteger(MIN_AVATAR_NUMBER, MAX_AVATAR_NUMBER) }.svg`,
   message: getRandomArrayElement(MESSAGES),
   name: getRandomArrayElement(NAMES),
 });
 
 // Создаем новый массив с комментариями
-const createArrayComments = () => new Array(getRandomInteger(0, 30)).fill(null).map((_element, index) => createComment(index));
+const createArrayComments = () => Array.from({length: getRandomInteger(MIN_QUANTITY_COMMENTS, MAX_QUANTITY_COMMENTS)}, createComment);
 
 // Создание объекта поста
-const createPost = (index) => ({
-  id: index,
-  url: `photos/${index}.jpg`,
+const createPost = () => ({
+  id: createPostId(),
+  url: `photos/${createUrlPhotoId()}.jpg`,
   description: getRandomArrayElement(DESCRIPTIONS),
-  likes: getRandomInteger(15, 200),
+  likes: getRandomInteger(MIN_QUANTITY_LIKES, MAX_QUANTITY_LIKES),
   comments: createArrayComments(),
 });
 
 // Создаём массив постов
-const arrayPosts = new Array(NUMBER_OF_POSTS).fill(null).map((_element, index) => createPost(index + 1));
+const arrayPosts = Array.from({length: NUMBER_OF_POSTS}, createPost);
 // eslint-disable-next-line no-console
 console.log(arrayPosts);
