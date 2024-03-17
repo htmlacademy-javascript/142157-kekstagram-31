e3a17e99cba70fbfbf9d47980858ead5f202df39
Denis Dyakonov <holosty@yandex.ru>
2024-03-17 15:59:46 +0300

    Открывается и закрывается

---
+++ js/big-picture.js
@@ -0,0 +1,23 @@
+import { postsData, renderThumbnails } from './thumbnails.js';
+import { renderComments } from './create-comments.js';
+
+renderThumbnails();
+
+const bigPicturePreview = document.querySelector('.big-picture__preview');
+const bigPictureImg = bigPicturePreview.querySelector('img');
+const bigPictureLikes = bigPicturePreview.querySelector('.likes-count');
+const bigPictureTotalComments = bigPicturePreview.querySelector('.social__comment-total-count');
+
+const createBigPicture = (index) => {
+
+  const element = postsData[index];
+  bigPictureImg.src = element.url;
+  bigPictureImg.alt = element.description;
+  bigPictureLikes.textContent = element.likes;
+  bigPictureTotalComments.textContent = element.comments.length;
+
+  const elementComments = element.comments;
+  renderComments(elementComments);
+};
+
+export { createBigPicture };
